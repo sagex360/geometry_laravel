b4a2730ec11cab5b95704b478f7a475c662260f2
Oleksandr Andriichuk <sagex360@gmail.com>
2019-10-11 16:14:37 +0300

    init

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+import App from './components/App';
+
+const app = new Vue({
+    el: '#app',
+    components: { App },
+});
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:400,500,700,400italic|Material+Icons">
+
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content" id="app">
+                <app></app>
+            </div>
+        </div>
+
+        <script src="{{ mix('js/bootstrap.js') }}"></script>
+        <script src="{{ mix('js/app.js') }}"></script>
+    </body>
+</html>
